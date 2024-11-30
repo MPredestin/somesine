b4d503df7ae0088c5479e1ca3214c53c2ba69c21
papyr <micpres354@gmail.com>
2024-11-29 23:31:29 -0500

    new commit

--- sine.py
+++ sine.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def plot_sine_wave(frequency=1, amplitude=1, duration=2, sampling_rate=1000):
 
     t = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)  # Time array
-    y = amplitude * np.sin(2 * np.pi * frequency * t)  # Sine wave formula
+    y = amplitude * np.sin(1 * np.pi * frequency * t)  # Sine wave formula
 
     plt.figure(figsize=(10, 4))
     plt.plot(t, y, label=f'{frequency} Hz Sine Wave')
